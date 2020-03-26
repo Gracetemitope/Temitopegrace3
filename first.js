d3c01b12c138acdcd54ec3d8b9840dba4aff6e33
gracetemitope <gracetemitope14@gmail.com>
2020-03-26 13:50:09 -0700

    Commit

---
+++ first.js
@@ -0,0 +1,16 @@
+let myName = "Temitope";
+let courses = [
+    "HTMl",
+    "CSS",
+    "JavaScript",
+];
+let sum = myName + ":" + " " + courses;
+console.log(sum);
+
+// displaying odd number because the number of courses I'm offering is odd
+
+for(let i = 1; i <= 200; i++){
+if (i % 2 !==0)
+
+console.log(i);
+}
